0c1c1929cd2b00d3642aca777801414571cdc228
ElhamMoharrami <elham.moharrami.em@gmail.com>
2023-06-08 16:08:10 +0330

    load every 1000 transaction to database/show time taken for loading every 1000 file

--- src/main/java/com/elham/bankproject/loader/MainLoader.java
+++ src/main/java/com/elham/bankproject/loader/MainLoader.java
@@ -13,7 +13,6 @@ public class MainLoader {
 
     public static void main(String[] args) {
         try {
-            long startTimeMillis = System.currentTimeMillis();
             ConfigLoader loadConfig = new ConfigLoader();
             String fileLoc = loadConfig.loadConfig("files.destination");
             CustomerTableCreator createCustomerTable = new CustomerTableCreator();
@@ -23,18 +22,36 @@ public class MainLoader {
             TransactionTableCreator createTransactionTable = new TransactionTableCreator();
             createTransactionTable.createTable();
             CsvReader csvReader = new CsvReader();
+            long startCustomerLoadTimeMillis = System.currentTimeMillis();
             List<String> customersList = csvReader.readFile(fileLoc + "/customers.csv");
             CustomerLoader loadCustomers = new CustomerLoader();
             loadCustomers.load(customersList);
+            long endCustomerLoadTimeMillis = System.currentTimeMillis();
+            long timeToLoadCustomers = endCustomerLoadTimeMillis - startCustomerLoadTimeMillis;
+            logger.info("customers loaded successfully. took "+timeToLoadCustomers+" milli seconds.");
+            long startAccountLoadTimeMillis = System.currentTimeMillis();
             List<String> accountsList = csvReader.readFile(fileLoc + "/accounts.csv");
             AccountLoader loadAccounts = new AccountLoader();
             loadAccounts.load(accountsList);
-            List<String> transactionList = csvReader.readFile(fileLoc + "/transaction.csv");
-            TransactionLoader loadTransactions = new TransactionLoader();
-            loadTransactions.load(transactionList);
-            long endTimeMillis = System.currentTimeMillis();
-            long timeToLoad = endTimeMillis - startTimeMillis;
-            logger.info("loading files to database took " + timeToLoad);
+            long endLoadLoadTimeMillis = System.currentTimeMillis();
+            long timeToLoadAccounts = endLoadLoadTimeMillis  - startAccountLoadTimeMillis;
+            logger.info("accounts loaded successfully. took "+timeToLoadAccounts+" milli seconds.");
+            int count = 0;
+            boolean flag = true;
+            while (flag) {
+                List<String> transactionList = csvReader.readFile(fileLoc + "/transaction" + count + ".csv");
+                if (transactionList.size() >1) {
+                    long startTransactionLoadTimeMillis = System.currentTimeMillis();
+                    TransactionLoader loadTransactions = new TransactionLoader();
+                    loadTransactions.load(transactionList);
+                    long endTransactionLoadTimeMillis = System.currentTimeMillis();
+                    long timeToLoadTransactions = endTransactionLoadTimeMillis - startTransactionLoadTimeMillis;
+                    logger.info("transaction file "+count+" loaded successfully. took "+timeToLoadTransactions+" milli seconds");
+                    count++;
+                } else {
+                    flag = false;
+                }
+            }
         } catch (ArrayIndexOutOfBoundsException e) {
             logger.warn("please enter a valid path to config.properties");
         }
