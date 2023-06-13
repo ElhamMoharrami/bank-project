762c81e1f30a56502a2f9febb110413046db773f
ElhamMoharrami <elham.moharrami.em@gmail.com>
2023-06-13 15:44:41 +0330

    implement sequential and threaded loading

--- src/main/java/com/elham/bankproject/loader/DataLoader.java
+++ src/main/java/com/elham/bankproject/loader/DataLoader.java
@@ -15,6 +15,7 @@ public class DataLoader {
         try {
             ConfigLoader loadConfig = new ConfigLoader();
             String fileLoc = loadConfig.loadConfig("files.destination");
+            int threadOrSequential = Integer.parseInt(loadConfig.loadConfig("loader.thread.count"));
             TableCreator tableCreatorC = TableCreator.addTablesToDb("customers");
             tableCreatorC.createTable();
             TableCreator tableCreatorA = TableCreator.addTablesToDb("accounts");
@@ -22,38 +23,78 @@ public class DataLoader {
             TableCreator tableCreatorT = TableCreator.addTablesToDb("transactions");
             tableCreatorT.createTable();
             CsvReader csvReader = new CsvReader();
-            long startCustomerLoadTimeMillis = System.currentTimeMillis();
-            List<String> customersList = csvReader.readFile(fileLoc + "/customers.csv");
-            TableLoader tableLoaderC = TableLoader.loadToDb("customers", customersList);
-            tableLoaderC.setSql("customers");
-            tableLoaderC.load();
-            long endCustomerLoadTimeMillis = System.currentTimeMillis();
-            long timeToLoadCustomers = endCustomerLoadTimeMillis - startCustomerLoadTimeMillis;
-            logger.info("customers loaded successfully. took " + timeToLoadCustomers + " milli seconds.");
-            long startAccountLoadTimeMillis = System.currentTimeMillis();
-            List<String> accountsList = csvReader.readFile(fileLoc + "/accounts.csv");
-            TableLoader tableLoaderA = TableLoader.loadToDb("accounts", accountsList);
-            tableLoaderA.setSql("accounts");
-            tableLoaderA.load();
-            long endLoadLoadTimeMillis = System.currentTimeMillis();
-            long timeToLoadAccounts = endLoadLoadTimeMillis - startAccountLoadTimeMillis;
-            logger.info("accounts loaded successfully. took " + timeToLoadAccounts + " milli seconds.");
-            int count = 0;
-            boolean flag = true;
-            while (flag) {
-                List<String> transactionList = csvReader.readFile(fileLoc + "/transaction" + count + ".csv");
-                if (transactionList.size() > 1) {
-                    long startTransactionLoadTimeMillis = System.currentTimeMillis();
-                    TableLoader tableLoaderT = TableLoader.loadToDb("transactions", transactionList);
-                    tableLoaderT.setSql("transactions");
-                    tableLoaderT.load();
-                    long endTransactionLoadTimeMillis = System.currentTimeMillis();
-                    long timeToLoadTransactions = endTransactionLoadTimeMillis - startTransactionLoadTimeMillis;
-                    logger.info("transaction file " + count + " loaded successfully. took " + timeToLoadTransactions +
-                            " milli seconds");
-                    count++;
-                } else {
-                    flag = false;
+            if (threadOrSequential == 0) {
+                long startCustomerLoadTimeMillis = System.currentTimeMillis();
+                List<String> customersList = csvReader.readFile(fileLoc + "/customers.csv");
+                TableLoader tableLoaderC = TableLoader.loadToDb("customers", customersList);
+                tableLoaderC.setSql("customers");
+                tableLoaderC.load();
+                long endCustomerLoadTimeMillis = System.currentTimeMillis();
+                long timeToLoadCustomers = endCustomerLoadTimeMillis - startCustomerLoadTimeMillis;
+                logger.info("customers loaded successfully. took " + timeToLoadCustomers + " milli seconds.");
+                long startAccountLoadTimeMillis = System.currentTimeMillis();
+                List<String> accountsList = csvReader.readFile(fileLoc + "/accounts.csv");
+                TableLoader tableLoaderA = TableLoader.loadToDb("accounts", accountsList);
+                tableLoaderA.setSql("accounts");
+                tableLoaderA.load();
+                long endLoadLoadTimeMillis = System.currentTimeMillis();
+                long timeToLoadAccounts = endLoadLoadTimeMillis - startAccountLoadTimeMillis;
+                logger.info("accounts loaded successfully. took " + timeToLoadAccounts + " milli seconds.");
+                int count = 0;
+                boolean flag = true;
+                while (flag) {
+                    List<String> transactionList = csvReader.readFile(fileLoc + "/transaction" + count + ".csv");
+                    if (transactionList.size() > 1) {
+                        long startTransactionLoadTimeMillis = System.currentTimeMillis();
+                        TableLoader tableLoaderT = TableLoader.loadToDb("transactions", transactionList);
+                        tableLoaderT.setSql("transactions");
+                        tableLoaderT.load();
+                        long endTransactionLoadTimeMillis = System.currentTimeMillis();
+                        long timeToLoadTransactions = endTransactionLoadTimeMillis - startTransactionLoadTimeMillis;
+                        logger.info("transaction file " + count + " loaded successfully. took " + timeToLoadTransactions +
+                                " milli seconds");
+                        count++;
+                    } else {
+                        flag = false;
+                    }
+                }
+            } else if (threadOrSequential == 1) {
+                long startCustomerLoadTimeMillis = System.currentTimeMillis();
+                List<String> customersList = csvReader.readFile(fileLoc + "/customers.csv");
+                TableLoader tableLoaderC = TableLoader.loadToDb("customers", customersList);
+                tableLoaderC.setSql("customers");
+                Thread loadC = new Thread(() -> tableLoaderC.load());
+                loadC.start();
+                long endCustomerLoadTimeMillis = System.currentTimeMillis();
+                long timeToLoadCustomers = endCustomerLoadTimeMillis - startCustomerLoadTimeMillis;
+                logger.info("customers loaded successfully. took " + timeToLoadCustomers + " milli seconds.");
+                long startAccountLoadTimeMillis = System.currentTimeMillis();
+                List<String> accountsList = csvReader.readFile(fileLoc + "/accounts.csv");
+                TableLoader tableLoaderA = TableLoader.loadToDb("accounts", accountsList);
+                tableLoaderA.setSql("accounts");
+                Thread loadA = new Thread(() -> tableLoaderA.load());
+                loadA.start();
+                long endLoadLoadTimeMillis = System.currentTimeMillis();
+                long timeToLoadAccounts = endLoadLoadTimeMillis - startAccountLoadTimeMillis;
+                logger.info("accounts loaded successfully. took " + timeToLoadAccounts + " milli seconds.");
+                int count = 0;
+                boolean flag = true;
+                while (flag) {
+                    List<String> transactionList = csvReader.readFile(fileLoc + "/transaction" + count + ".csv");
+                    if (transactionList.size() > 1) {
+                        long startTransactionLoadTimeMillis = System.currentTimeMillis();
+                        TableLoader tableLoaderT = TableLoader.loadToDb("transactions", transactionList);
+                        tableLoaderT.setSql("transactions");
+                        Thread loadT = new Thread(() -> tableLoaderT.load());
+                        loadT.start();
+                        long endTransactionLoadTimeMillis = System.currentTimeMillis();
+                        long timeToLoadTransactions = endTransactionLoadTimeMillis - startTransactionLoadTimeMillis;
+                        logger.info("transaction file " + count + " loaded successfully. took " + timeToLoadTransactions +
+                                " milli seconds");
+                        count++;
+                    } else {
+                        flag = false;
+                    }
                 }
             }
         } catch (ArrayIndexOutOfBoundsException e) {
